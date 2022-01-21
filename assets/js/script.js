6987bfacf933f366e970f4d8de29bde7c8933bac
Oscar Troncoso <oscarleandro1595@gmail.com>
2022-01-20 22:39:31 -0300

    realizados los scripts y componentes bootstrap version 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,37 @@
 $(()=>{
-    
+
+    //Habilita el uso de tooltip para bootstrap 5
+
     var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
         var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
         return new bootstrap.Tooltip(tooltipTriggerEl)
     });
 
-    $('#enviarCorreo').click(()=>{
-        alert('El correo ha sido enviado exitosamente');
-    });
-
-    $('#ingrediente').on("dblclick", ()=>{
-       $('#ingrediente ').css({'color': 'red'})
+    
+    //Envia un alert al enviar el mensaje de contacto
+    $('#enviarMensaje').click(()=>{
+        
+        alert('El Mensaje ha sido enviado exitosamente');
+        
     });
     
-    //titulo preparacion
-    $('#preparacion').on("dblclick", ()=>{
-        $('#preparacion ').css({'color': 'red'})
-    });
 
+    //Al presionar el titulo de las card realiza la animacion toggle 
     $('.card-title').click(
         ()=>{
-            $('.card-text').toggle()
+            $('.card-text').animate({'height':'toggle'},'slow')
 
         }
     )
+
+    //Al hacer scroll el Navbar cambia de transparente a color desde los 50px hacia abajo
+    $(window).scroll(()=>{
+        $('nav').toggleClass('scrolled', $(this).scrollTop() > 50)
+    });
+
+    
+
+    
 
      
 
